ced4f1d28d290d8443d30c690212d02e807f664a
Slava Skryabin <vs8199@gmail.com>
2020-03-05 17:30:25 -0800

    libs update

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -1,4 +1,4 @@
-// Created by Viacheslav (Slava) Skryabin 04/01/2018
+// Created by Viacheslav (Slava) Skryabin 04/01/2011
 package support;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -8,13 +8,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
@@ -37,7 +35,6 @@ public class TestContext {
     }
 
     public static void initialize(String browser, boolean isHeadless) {
-        String osName = System.getProperty("os.name");
         switch (browser) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
@@ -45,7 +42,6 @@ public class TestContext {
                 chromePreferences.put("profile.default_content_settings.geolocation", 2);
                 chromePreferences.put("download.prompt_for_download", false);
                 chromePreferences.put("download.directory_upgrade", true);
-                chromePreferences.put("download.default_directory", getDownloadsPath());
                 chromePreferences.put("credentials_enable_service", false);
                 chromePreferences.put("password_manager_enabled", false);
                 chromePreferences.put("safebrowsing.enabled", "true");
@@ -61,20 +57,7 @@ public class TestContext {
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
-                System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
-                System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-                FirefoxProfile firefoxProfile = new FirefoxProfile();
-                firefoxProfile.setPreference("xpinstall.signatures.required", false);
-                firefoxProfile.setPreference("app.update.enabled", false);
-                firefoxProfile.setPreference("browser.download.folderList", 2);
-                firefoxProfile.setPreference("browser.download.manager.showWhenStarting", false);
-                firefoxProfile.setPreference("browser.download.dir", getDownloadsPath());
-                firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/zip;application/octet-stream;application/x-zip;application/x-zip-compressed;text/css;text/html;text/plain;text/xml;text/comma-separated-values");
-                firefoxProfile.setPreference("browser.helperApps.neverAsk.openFile", "application/zip;application/octet-stream;application/x-zip;application/x-zip-compressed;text/css;text/html;text/plain;text/xml;text/comma-separated-values");
-                firefoxProfile.setPreference("browser.helperApps.alwaysAsk.force", false);
-                firefoxProfile.setPreference("plugin.disable_full_page_plugi‌​n_for_types", "application/pdf,application/vnd.adobe.xfdf,application/vnd.‌​fdf,application/vnd.‌​adobe.xdp+xml");
-                firefoxProfile.setPreference("webdriver.log.driver", "OFF");
-                FirefoxOptions firefoxOptions = new FirefoxOptions().setProfile(firefoxProfile).setLogLevel(FirefoxDriverLogLevel.INFO);
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
                 if (isHeadless) {
                     FirefoxBinary firefoxBinary = new FirefoxBinary();
                     firefoxBinary.addCommandLineOptions("--headless");
@@ -88,11 +71,7 @@ public class TestContext {
                 break;
             case "ie":
                 WebDriverManager.iedriver().setup();
-                DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-                ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-                ieCapabilities.setCapability("requireWindowFocus", true);
-                InternetExplorerOptions ieOptions = new InternetExplorerOptions(ieCapabilities);
-                driver = new InternetExplorerDriver(ieOptions);
+                driver = new InternetExplorerDriver();
                 break;
             case "grid":
                 DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -110,13 +89,4 @@ public class TestContext {
                 throw new RuntimeException("Driver is not implemented for: " + browser);
         }
     }
-
-    private static String getDriversDirPath() {
-        return System.getProperty("user.dir") + String.format("%1$ssrc%1$stest%1$sresources%1$sdrivers%1$s", File.separator);
-    }
-
-    private static String getDownloadsPath() {
-        return System.getProperty("user.dir") + String.format("%1$ssrc%1$stest%1$sresources%1$sdownloads%1$s", File.separator);
-    }
-
 }
